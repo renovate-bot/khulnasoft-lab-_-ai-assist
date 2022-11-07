84188b5a58df11dbdb5ae500d018cd95c37e4da6
Alexander Chueshev <achueshev@gitlab.com>
2022-11-07 11:40:49 +0100

    Strip root folder when unzipping model

--- model-loader/load-model.sh
+++ model-loader/load-model.sh
@@ -8,8 +8,8 @@ MODEL_NUM_GPUS_NAME="${MODEL}-${NUM_GPUS}gpu"
 MODEL_ARCHIVE="${MODELS_DIR}/${MODEL_NUM_GPUS_NAME}.tar.zst"
 
 mkdir -p "${MODELS_DIR}"
-cp -r "${MODELS_CONFIG_DIR}/${MODEL_NUM_GPUS_NAME}" "${MODELS_DIR}/${MODEL_NUM_GPUS_NAME}"
+cp -r "${MODELS_CONFIG_DIR}/${MODEL_NUM_GPUS_NAME}/." "${MODELS_DIR}"
 
 curl -L "https://huggingface.co/moyix/${MODEL}-gptj/resolve/main/${MODEL}-${NUM_GPUS}gpu.tar.zst" -o "$MODEL_ARCHIVE"
-zstd -dc "$MODEL_ARCHIVE" | tar -xf - -C "${MODELS_DIR}"
+zstd -dc "$MODEL_ARCHIVE" | tar -xf - -C "${MODELS_DIR}" --strip-components=1
 rm -f "$MODEL_ARCHIVE"
