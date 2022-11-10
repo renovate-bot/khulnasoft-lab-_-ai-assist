526c1a7e7397120726aba23e0dcf6cf095e2b2d1
Fred de Gier <fdegier@gitlab.com>
2022-11-10 18:36:26 +0100

    Improve logging

--- api/app.py
+++ api/app.py
@@ -1,4 +1,5 @@
 import logging
+import hashlib
 
 import uvicorn
 from fastapi import FastAPI, Request, Response, HTTPException
@@ -37,29 +38,35 @@ async def completions(data: OpenAIinput, request: Request):
     # Trim whitespace from the stop sequences
     # TODO: move this to VS Code or preprocessing
     data['stop'] = [i.strip() for i in data['stop']]
+    user = hashlib.sha1(request.client.host.encode("utf-8")).hexdigest()
+    print(f"{user} - Received request")
 
     if gitlab.user_is_allowed(token=request.headers.get('authorization', '')):
         try:
             content = codegen(data=data)
         except codegen.TokensExceedsMaximum as E:
+            print(f"{user} - Tokens exceeded")
             raise HTTPException(
                 status_code=400,
                 detail=str(E),
             )
 
         if data.get("stream") is not None:
+            print(f"{user} - Returning suggestions")
             return EventSourceResponse(
                 content=content,
                 status_code=200,
                 media_type="text/event-stream"
             )
         else:
+            print(f"{user} - Returning suggestions")
             return Response(
                 status_code=200,
                 content=content,
                 media_type="application/json"
             )
     else:
+        print(f"{user} - Failed to get suggestions 404")
         return Response(
             status_code=404,
             content="{}",
