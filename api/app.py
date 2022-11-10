5d4aadeaaccdd08ade0a81dab99d2039301959ac
Fred de Gier <fdegier@gitlab.com>
2022-11-10 15:32:07 +0100

    Trim white space from stop sequences. Remove error handling as it was bugged

--- api/app.py
+++ api/app.py
@@ -1,13 +1,11 @@
 import logging
 
 import uvicorn
-from fastapi import FastAPI, Request, Response
-from fastapi.responses import JSONResponse
+from fastapi import FastAPI, Request, Response, HTTPException
 from sse_starlette.sse import EventSourceResponse
 
 from models import OpenAIinput
 from utils.codegen import CodeGenProxy
-from utils.errors import FauxPilotException
 from utils.gitlab import GitLab
 from config.config import Config
 
@@ -33,26 +31,20 @@ app = FastAPI(
 )
 
 
-@app.exception_handler(FauxPilotException)
-async def fauxpilot_handler(exc: FauxPilotException):
-    return JSONResponse(
-        status_code=400,
-        content=exc.json()
-    )
-
-
 @app.post("/v1/completions")
 async def completions(data: OpenAIinput, request: Request):
     data = data.dict()
+    # Trim whitespace from the stop sequences
+    # TODO: move this to VS Code or preprocessing
+    data['stop'] = [i.strip() for i in data['stop']]
+
     if gitlab.user_is_allowed(token=request.headers.get('authorization', '')):
         try:
             content = codegen(data=data)
         except codegen.TokensExceedsMaximum as E:
-            raise FauxPilotException(
-                message=str(E),
-                error_type="invalid_request_error",
-                param=None,
-                code=None,
+            raise HTTPException(
+                status_code=400,
+                detail=str(E),
             )
 
         if data.get("stream") is not None:
