8e60306ee965f8ddbd8c1f0d6df2e1afaa6dd512
Alexander Chueshev <achueshev@gitlab.com>
2022-12-21 22:27:18 +0100

    Implement DI container with the `code_suggestions_usecase` provider

--- codesuggestions/deps.py
+++ codesuggestions/deps.py
@@ -2,14 +2,23 @@ from dependency_injector import containers, providers
 
 from codesuggestions.auth import GitLabAuthProvider
 from codesuggestions.api import middleware
+from codesuggestions.models import grpc_connect_triton, Codegen
+from codesuggestions.suggestions import CodeSuggestionsUseCase
 
 __all__ = [
     "FastApiContainer",
+    "CodeSuggestionsContainer",
 ]
 
 
+def _init_triton_grpc_client(host: str, port: int):
+    client = grpc_connect_triton(host, port)
+    yield client
+    client.close()
+
+
 class FastApiContainer(containers.DeclarativeContainer):
-    wiring_config = containers.WiringConfiguration(modules=["__main__"])
+    wiring_config = containers.WiringConfiguration(modules=["codesuggestions.api.server"])
 
     config = providers.Configuration()
 
@@ -21,5 +30,27 @@ class FastApiContainer(containers.DeclarativeContainer):
     auth_middleware = providers.Factory(
         middleware.MiddlewareAuthentication,
         auth_provider,
-        bypass_auth=config.auth.bypass
+        bypass_auth=config.auth.bypass,
+    )
+
+
+class CodeSuggestionsContainer(containers.DeclarativeContainer):
+    wiring_config = containers.WiringConfiguration(modules=["codesuggestions.api.suggestions"])
+
+    config = providers.Configuration()
+
+    grpc_client = providers.Resource(
+        _init_triton_grpc_client,
+        host=config.triton.host,
+        port=config.triton.port,
+    )
+
+    model_codegen = providers.Singleton(
+        Codegen,
+        grpc_client=grpc_client,
+    )
+
+    usecase = providers.Singleton(
+        CodeSuggestionsUseCase,
+        model=model_codegen,
     )
