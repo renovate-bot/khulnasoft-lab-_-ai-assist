899d6f699c4c4a45d9ea7118af992920aa5d62d6
Alexander Chueshev <achueshev@gitlab.com>
2022-12-21 23:01:56 +0100

    Do not strip prompt in CodeSuggestionsUseCase

--- codesuggestions/suggestions/base.py
+++ codesuggestions/suggestions/base.py
@@ -10,21 +10,12 @@ class CodeSuggestionsUseCase:
         self.model = model
 
     # noinspection PyMethodMayBeStatic
-    def _rstrip_prompt(self, prompt: str) -> str:
-        return prompt.rstrip()
-
-    # noinspection PyMethodMayBeStatic
     def _preprocess_prompt(self, prompt: str, *funcs) -> str:
         for func in funcs:
             prompt = func(prompt)
         return prompt
 
     def __call__(self, prompt: str) -> str:
-        prompt = self._preprocess_prompt(
-            prompt,
-            self._rstrip_prompt,
-        )
-
         completion = self.model(prompt)
 
         return completion
