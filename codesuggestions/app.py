2c18364a628f6e17e92272c31051c27df17f37eb
Alexander Chueshev <achueshev@gitlab.com>
2022-12-22 16:36:08 +0100

    Start fastapi http server

--- codesuggestions/app.py
+++ codesuggestions/app.py
@@ -1,20 +1,34 @@
 import uvicorn
+from dotenv import load_dotenv
 
-from fastapi import FastAPI
+from codesuggestions import Config
+from codesuggestions.api import create_fast_api_server
+from codesuggestions.deps import FastApiContainer, CodeSuggestionsContainer
 
-from codesuggestions.api import http
-from codesuggestions.api import middleware
+load_dotenv()
 
-app = FastAPI(
-    title="GitLab Code Suggestions",
-    description="GitLab Code Suggestions API to serve code completion predictions",
-    middleware=[
-        middleware.MiddlewareAuthentication(),
-        middleware.MiddlewareLogRequest()
-    ],
-)
+config = Config()
+fast_api_container = FastApiContainer()
+fast_api_container.config.auth.from_value(config.auth._asdict())
+fast_api_container.config.fastapi.from_value(config.fastapi._asdict())
 
-app.include_router(http.router)
+code_suggestions_container = CodeSuggestionsContainer()
+code_suggestions_container.config.triton.from_value(config.triton._asdict())
+
+app = create_fast_api_server()
+
+
+@app.on_event("startup")
+def on_server_startup():
+    fast_api_container.init_resources()
+    code_suggestions_container.init_resources()
+
+
+@app.on_event("shutdown")
+def on_server_shutdown():
+    fast_api_container.shutdown_resources()
+    code_suggestions_container.shutdown_resources()
+
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=5052)
+    uvicorn.run("app:app", host=config.fastapi.api_host, port=config.fastapi.api_port)
