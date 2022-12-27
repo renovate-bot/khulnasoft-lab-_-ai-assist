afc252c0dd5eeeb0be71dd4f1dda3081c9ccf347
Alexander Chueshev <achueshev@gitlab.com>
2022-12-27 13:44:38 +0100

    Update request/response types for the `completions` endpoint

--- codesuggestions/api/suggestions.py
+++ codesuggestions/api/suggestions.py
@@ -1,3 +1,6 @@
+from typing import Optional, Union
+from time import time
+
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from dependency_injector.wiring import Provide, inject
@@ -15,22 +18,58 @@ router = APIRouter(
 )
 
 
+# most of the fields created for backward compatibility
 class RequestSuggestions(BaseModel):
-    prompt: str
+    model: str = "fastertransformer"
+    prompt: Optional[str]
+    suffix: Optional[str]
+    max_tokens: Optional[int] = 16
+    temperature: Optional[float] = 0.6
+    top_p: Optional[float] = 1.0
+    n: Optional[int] = 1
+    stream: Optional[bool]
+    logprobs: Optional[int] = None
+    echo: Optional[bool]
+    stop: Optional[Union[str, list]]
+    presence_penalty: Optional[float] = 0
+    frequency_penalty: Optional[float] = 1
+    best_of: Optional[int] = 1
+    logit_bias: Optional[dict]
+    user: Optional[str]
 
 
 class ResponseSuggestions(BaseModel):
-    content: str
+    # created for backward compatibility
+    class Usage(BaseModel):
+        completion_tokens: int
+        prompt_tokens: int
+        total_tokens: int
+
+    class Choice(BaseModel):
+        text: str
+        index: int = 0
+        finish_reason: str = "length"
+
+    id: str
+    model: str = "codegen"
+    object: str = 'text_completion'
+    created: int
+    choices: list[Choice]
+    usage: Optional[Usage]
 
 
 @router.post("/", response_model=ResponseSuggestions)
 @inject
-def completions(
+async def completions(
         req: RequestSuggestions,
         code_suggestions: CodeSuggestionsUseCase = Depends(Provide[CodeSuggestionsContainer.usecase]),
 ):
-    suggestions = code_suggestions(req.prompt)
+    suggestion = code_suggestions(req.prompt)
 
     return ResponseSuggestions(
-        content=suggestions,
+        id="id",
+        created=int(time()),
+        choices=[
+            ResponseSuggestions.Choice(text=suggestion),
+        ],
     )
