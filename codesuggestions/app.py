5b9cb37ce9f03253d986725d50b2cf58f1006222
Alexander Chueshev <achueshev@gitlab.com>
2022-12-27 22:27:31 +0100

    Wrap server creation into the main function

--- codesuggestions/app.py
+++ codesuggestions/app.py
@@ -6,33 +6,38 @@ from codesuggestions import Config
 from codesuggestions.api import create_fast_api_server
 from codesuggestions.deps import FastApiContainer, CodeSuggestionsContainer
 
+# load env variables from .env if exists
 load_dotenv()
+
+# prepare configuration settings
 config = Config()
 
 # configure logging
 dictConfig(config.fastapi.uvicorn_logger)
 
-fast_api_container = FastApiContainer()
-fast_api_container.config.auth.from_value(config.auth._asdict())
-fast_api_container.config.fastapi.from_value(config.fastapi._asdict())
 
-code_suggestions_container = CodeSuggestionsContainer()
-code_suggestions_container.config.triton.from_value(config.triton._asdict())
+def main():
+    fast_api_container = FastApiContainer()
+    fast_api_container.config.auth.from_value(config.auth._asdict())
+    fast_api_container.config.fastapi.from_value(config.fastapi._asdict())
 
-app = create_fast_api_server()
+    code_suggestions_container = CodeSuggestionsContainer()
+    code_suggestions_container.config.triton.from_value(config.triton._asdict())
 
+    app = create_fast_api_server()
 
-@app.on_event("startup")
-def on_server_startup():
-    fast_api_container.init_resources()
-    code_suggestions_container.init_resources()
+    @app.on_event("startup")
+    def on_server_startup():
+        fast_api_container.init_resources()
+        code_suggestions_container.init_resources()
 
+    @app.on_event("shutdown")
+    def on_server_shutdown():
+        fast_api_container.shutdown_resources()
+        code_suggestions_container.shutdown_resources()
 
-@app.on_event("shutdown")
-def on_server_shutdown():
-    fast_api_container.shutdown_resources()
-    code_suggestions_container.shutdown_resources()
+    uvicorn.run(app, host=config.fastapi.api_host, port=config.fastapi.api_port)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host=config.fastapi.api_host, port=config.fastapi.api_port)
+    main()
