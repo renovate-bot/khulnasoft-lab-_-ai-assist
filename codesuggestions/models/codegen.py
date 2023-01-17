606736ae627d2f7b284c8bfa0bc36f19528ba0c5
Alexander Chueshev <achueshev@gitlab.com>
2023-01-17 15:53:38 +0100

    Update model io to match the FauxPilot requirements

--- codesuggestions/models/codegen.py
+++ codesuggestions/models/codegen.py
@@ -15,7 +15,17 @@ class Codegen(BaseModel):
     MAX_MODEL_LEN = 2048
 
     # Model name specified in config.pbtxt
-    MODEL_NAME = "codegen"
+    MODEL_NAME = "ensemble"
+
+    # Number of tokens to generate
+    REQUEST_OUTPUT_LEN = 16
+
+    # Model hyperparameters
+    MODEL_TEMPERATURE = .2
+    MODEL_REPETITION_PENALTY = 1.1
+    MODEL_TOP_K = 0
+    MODEL_TOP_P = .95
+    MODEL_PAD_ID = 50256
 
     def __init__(self, grpc_client: triton_grpc_util.InferenceServerClient, timeout: int = 10):
         self.client = grpc_client
@@ -25,28 +35,58 @@ class Codegen(BaseModel):
     def _trim_prompt_max_len(self, prompt: str) -> str:
         return prompt[-Codegen.MAX_MODEL_LEN:]
 
+    def _model_input(self, prompt: str) -> list:
+        prompt_np = np.array([[prompt]], dtype=object)
+        request_output_len_np = np.array([[self.REQUEST_OUTPUT_LEN]]).astype(np.uint32)
+        temperature_np = np.array([[self.MODEL_TEMPERATURE]]).astype(np.float32)
+        repetition_penalty_np = np.array([[self.MODEL_REPETITION_PENALTY]]).astype(np.float32)
+        top_k_np = np.array([[self.MODEL_TOP_K]]).astype(np.uint32)
+        top_p_np = np.array([[self.MODEL_TOP_P]]).astype(np.float32)
+        start_id_np = np.array([[self.MODEL_PAD_ID]]).astype(np.uint32)
+        end_id_np = np.array([[self.MODEL_PAD_ID]]).astype(np.uint32)
+        random_seed_np = np.random.randint(0, 2 ** 31 - 1, (1, 1), dtype=np.int32)
+
+        return [
+            grpc_input_from_np("prompt", prompt_np),
+            grpc_input_from_np("request_output_len", request_output_len_np),
+            grpc_input_from_np("temperature", temperature_np),
+            grpc_input_from_np("repetition_penalty", repetition_penalty_np),
+            grpc_input_from_np("runtime_top_k", top_k_np),
+            grpc_input_from_np("runtime_top_p", top_p_np),
+            grpc_input_from_np("start_id", start_id_np),
+            grpc_input_from_np("end_id", end_id_np),
+            grpc_input_from_np("random_seed", random_seed_np),
+            grpc_input_from_np("is_return_log_probs", np.array([[1]]).astype(bool)),
+        ]
+
+    # noinspection PyMethodMayBeStatic
+    def _model_outputs(self) -> list:
+        return [
+            grpc_requested_output("completions"),
+            grpc_requested_output("sequence_length"),
+            grpc_requested_output("output_log_probs"),
+            grpc_requested_output("cum_log_probs"),
+            grpc_requested_output("output_ids"),
+        ]
+
     def __call__(self, prompt: str) -> str:
         prompt = self._trim_prompt_max_len(prompt)
-        prompts_np = np.array([[prompt]], dtype=object)
 
-        inputs_model = [
-            grpc_input_from_np("prompts", prompts_np)
-        ]
-        outputs_model = [
-            grpc_requested_output("completions"),
-        ]
+        inputs_model = self._model_input(prompt)
+        outputs_model = self._model_outputs()
 
         response = self.client.infer(
             Codegen.MODEL_NAME,
             inputs_model,
             request_id=uuid.uuid4().hex,
-            client_timeout=self.timeout,
             outputs=outputs_model,
         )
 
-        completions = (
+        completion = (
             response.as_numpy("completions")[0]
             .decode("utf-8")
         )
 
-        return completions
+        completion = completion[len(prompt):]
+
+        return completion
