5c00c34037a60d82076888e262658c7077e29cc7
Andrew Newdigate <andrew@gitlab.com>
2023-05-26 09:59:25 +0200

    updates to model

--- infrastructure/render.sh
+++ infrastructure/render.sh
@@ -16,11 +16,37 @@ helm template ai-assist ai-assist --debug | gawk -v "tempdir=$tempdir" '
   }
 '
 
+function find_target_for() {
+  if [[ -f ../manifests/fauxpilot/v2/$1 ]]; then
+    echo ../manifests/fauxpilot/v2/$1
+    return
+  fi
+
+  if [[ -f ../manifests/fauxpilot/$1 ]]; then
+    echo ../manifests/fauxpilot/$1
+    return
+  fi
+
+  find ../manifests -name "$base" -not -path "**/codegen**" | head -1
+}
+
+git restore --source=origin/main --staged --worktree -- ../manifests
+
 for i in ${tempdir}/*.yaml; do
   base=$(basename "$i")
-  target=$(find ../manifests -name "$base" -not -path "**/codegen**" | head -1)
-  cp "$i" "$target"
-  echo "$target"
+
+  target=$(find_target_for "${base}")
+  echo "$i -> $target"
+
+  tempdir2=$(mktemp -d)
+  (
+    cd "$tempdir2"
+    yq --split-exp '.kind + "-" + $index' "$i"
+    cat *.yml
+  ) >"$target"
+  rm -rf "$tempdir2"
+
+  # cp "$i" "$target"
 done
 
 yamlfmt '../manifests/**.yaml'
