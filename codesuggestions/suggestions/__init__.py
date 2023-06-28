fd826e93fd09036dd858b1c76a74f1cf56e8fc5d
Alexander Chueshev <achueshev@gitlab.com>
2023-06-28 11:39:23 +0200

    Implement CodeCompletionsInternalUseCase

--- codesuggestions/suggestions/__init__.py
+++ codesuggestions/suggestions/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa
 
 from codesuggestions.suggestions.base import *
+from codesuggestions.suggestions.internal import *
---
+++ codesuggestions/suggestions/internal.py
@@ -0,0 +1,50 @@
+from typing import Optional, Any, NamedTuple
+
+from starlette_context import context
+
+from codesuggestions.suggestions.processing import ModelEngineBase
+
+__all__ = [
+    "CodeCompletionsInternalUseCase",
+]
+
+
+class CodeCompletionsInternal(NamedTuple):
+    class Model(NamedTuple):
+        # TODO: replace with enum values
+        engine: str
+        name: str
+
+    text: str
+    model: Model
+    finish_reason: str = "length"
+
+
+class CodeCompletionsInternalUseCase:
+    def __init__(self, engine: ModelEngineBase):
+        self.engine = engine
+
+    def __call__(
+        self,
+        prefix: str,
+        suffix: str,
+        file_name: Optional[str] = None,
+        **kwargs: Any
+    ) -> CodeCompletionsInternal:
+        file_name = file_name if file_name else ""
+
+        completion = self.engine.generate_completion(
+            prefix,
+            suffix,
+            file_name,
+            **kwargs,
+        )
+
+        return CodeCompletionsInternal(
+            text=completion,
+            model=CodeCompletionsInternal.Model(
+                # TODO: return props from the target engine instead of using glob var
+                engine=context.get("model_engine", ""),
+                name=context.get("model_name", "")
+            )
+        )
