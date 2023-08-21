db68fe7c27e15fb02246b3735f60a55f5e91bbb9
Bruno Cardoso <bcardoso@gitlab.com>
2023-08-21 16:14:53 +0200

    Rename exp_truncate_suffix_python to exp_truncate_suffix

--- codesuggestions/experimentation/provider.py
+++ codesuggestions/experimentation/provider.py
@@ -1,12 +1,12 @@
 from dependency_injector import providers
 
 from codesuggestions.experimentation.base import Experiment, ExperimentRegistry
-from codesuggestions.experimentation.experiments import exp_truncate_suffix_python
+from codesuggestions.experimentation.experiments import exp_truncate_suffix
 
 
 def _experiments_provider() -> list[Experiment]:
     return providers.List(
-        exp_truncate_suffix_python.make_experiment(),
+        exp_truncate_suffix.make_experiment(),
     )
 
 
