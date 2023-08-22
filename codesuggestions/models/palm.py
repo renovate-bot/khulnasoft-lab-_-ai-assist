98642ad89affd721434fa65d1bb287ce29243705
Alexander Chueshev <achueshev@gitlab.com>
2023-08-22 18:30:21 +0200

    Increase the default max_output_tokens for code-bison to 2048

--- codesuggestions/models/palm.py
+++ codesuggestions/models/palm.py
@@ -226,7 +226,7 @@ class PalmCodeBisonModel(PalmCodeGenBaseModel):
         prompt: str,
         suffix: str,
         temperature: float = 0.2,
-        max_output_tokens: int = 32,
+        max_output_tokens: int = 2048,
         top_p: float = 0.95,
         top_k: int = 40,
     ) -> Optional[TextGenModelOutput]:
