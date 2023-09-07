c20b019c6453ccd022bfd82112e42385b4aace53
Alexander Chueshev <achueshev@gitlab.com>
2023-09-07 17:06:06 +0200

    Fix tests

--- codesuggestions/suggestions/processing/__init__.py
+++ codesuggestions/suggestions/processing/__init__.py
@@ -1,8 +1,7 @@
 # flake8: noqa
 
-from codesuggestions.suggestions.processing import ops
+from codesuggestions.suggestions.processing import ops, post
 from codesuggestions.suggestions.processing.base import *
 from codesuggestions.suggestions.processing.completions import *
 from codesuggestions.suggestions.processing.generations import *
-from codesuggestions.suggestions.processing.post import *
 from codesuggestions.suggestions.processing.typing import *
