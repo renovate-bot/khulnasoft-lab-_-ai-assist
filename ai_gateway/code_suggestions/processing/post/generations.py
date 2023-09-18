83da52238ed4c4f463c981f9bcdae3566f2340f2
Alexander Chueshev <achueshev@gitlab.com>
2023-09-18 13:08:21 +0200

    Clean model refection for code generation

--- ai_gateway/code_suggestions/processing/post/generations.py
+++ ai_gateway/code_suggestions/processing/post/generations.py
@@ -1,7 +1,9 @@
 from typing import Any
 
+from ai_gateway.code_suggestions.processing.ops import strip_whitespaces
 from ai_gateway.code_suggestions.processing.post.base import PostProcessorBase
 from ai_gateway.code_suggestions.processing.post.ops import (
+    clean_model_reflection,
     prepend_new_line,
     strip_code_block_markdown,
 )
@@ -18,5 +20,10 @@ class PostProcessor(PostProcessorBase):
     def process(self, completion: str, **kwargs: Any) -> str:
         completion = strip_code_block_markdown(completion)
         completion = prepend_new_line(self.code_context, completion)
+
+        # Note: `clean_model_reflection` joins code context and completion
+        # we need to call the function right after prepending a new line
+        completion = clean_model_reflection(self.code_context, completion)
+        completion = strip_whitespaces(completion)
 
         return completion
