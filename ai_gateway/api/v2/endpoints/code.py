7042da0ad745bc827219cb3998ada28112ed96b4
Tim Zallmann <tzallmann@gitlab.com>
2023-09-19 12:43:47 +0200

    Fix for prompt existing

--- ai_gateway/api/v2/endpoints/code.py
+++ ai_gateway/api/v2/endpoints/code.py
@@ -154,7 +154,7 @@ async def generations(
 
     log.debug(
         "code creation input:",
-        prompt=payload.prompt,
+        prompt=payload.prompt if hasattr(payload, "prompt") else None,
         prefix=payload.current_file.content_above_cursor,
         suffix=payload.current_file.content_below_cursor,
         current_file_name=payload.current_file.file_name,
