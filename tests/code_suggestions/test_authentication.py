7fbf9702a352b84b20519d332141210210ef5433
Tan Le <tle@gitlab.com>
2023-10-26 19:38:39 +1100

    Return the correct scopes when bypassing auth
    Developers can opt in to disable authentication for testing by setting
    AUTH_BYPASS_EXTERNAL=true. Since permission scope is introduced, the
    authentication credentials for this use case miss the scopes value. This
    results in Forbidden error and prohibit us to use the particular flag.

--- tests/code_suggestions/test_authentication.py
+++ tests/code_suggestions/test_authentication.py
@@ -2,13 +2,15 @@ import json
 
 import pytest
 from dependency_injector.wiring import inject
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, FastAPI, Request
 from fastapi.testclient import TestClient
 from starlette.authentication import requires
+from starlette.middleware import Middleware
 from starlette.responses import JSONResponse
 from starlette.routing import Route
 from structlog.testing import capture_logs
 
+from ai_gateway.api.middleware import MiddlewareAuthentication
 from ai_gateway.auth import User, UserClaims
 
 router = APIRouter(
@@ -20,7 +22,14 @@ router = APIRouter(
 @router.post("/")
 @requires("code_suggestions")
 def homepage(request: Request):
-    return JSONResponse(status_code=200, content={"scopes": request.auth.scopes})
+    return JSONResponse(
+        status_code=200,
+        content={
+            "authenticated": request.user.is_authenticated,
+            "is_debug": request.user.is_debug,
+            "scopes": request.auth.scopes,
+        },
+    )
 
 
 @pytest.fixture(scope="class")
@@ -114,7 +123,7 @@ invalid_authentication_token_type_error = {
                     is_third_party_ai_default=False, scopes=["code_suggestions"]
                 ),
             ),
-            {"scopes": ["code_suggestions"]},
+            {"authenticated": True, "is_debug": False, "scopes": ["code_suggestions"]},
             ["auth_duration_s"],
         ),
         (
@@ -166,3 +175,20 @@ def test_failed_authorization_logging(
         assert cap_logs[0]["status_code"] == expected_status_code
         assert cap_logs[0]["method"] == "POST"
         assert set(cap_logs[0].keys()) == set(expected_log_keys + log_keys)
+
+
+def test_bypass_auth(fast_api_router, stub_auth_provider):
+    middlewares = [
+        MiddlewareAuthentication(stub_auth_provider, True, None),
+    ]
+    app = FastAPI(middleware=middlewares)
+    app.include_router(fast_api_router)
+    mock_client = TestClient(app)
+
+    response = mock_client.post("/")
+
+    assert response.json() == {
+        "authenticated": True,
+        "is_debug": True,
+        "scopes": ["code_suggestions"],
+    }
