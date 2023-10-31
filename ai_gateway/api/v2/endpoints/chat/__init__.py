30d0cc83e3164d1f9cb53d16e277b742713d7179
Alexander Chueshev <achueshev@gitlab.com>
2023-10-31 18:25:48 +0100

    Deploy the chat evaluation API when F_CHAT_EVALUATION_API is True

--- ai_gateway/api/v2/endpoints/chat/__init__.py
+++ ai_gateway/api/v2/endpoints/chat/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import APIRouter
 
 from ai_gateway.api.v2.endpoints.chat import evaluation
@@ -6,4 +8,7 @@ __all__ = ["router"]
 
 router = APIRouter()
 
-router.include_router(evaluation.router, prefix="/evaluation")
+# Hack: Avoid deploying the chat evaluation endpoints to production
+# TODO: Protect the API endpoints verifying the token scopes
+if os.environ.get("F_CHAT_EVALUATION_API", "0").lower() in ("1", "true"):
+    router.include_router(evaluation.create_router(), prefix="/evaluation")
