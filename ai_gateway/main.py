39524b4c229232eb4880ce552c61b476c0e0c7b3
michaelangeloio <arivera@gitlab.com>
2024-03-21 21:16:15 -0400

    add back comment

--- ai_gateway/main.py
+++ ai_gateway/main.py
@@ -4,6 +4,8 @@ from ai_gateway.app import get_config
 
 def run_app():
     config = get_config()
+    
+    # For now, trust all IPs for proxy headers until https://github.com/encode/uvicorn/pull/1611 is available.
     uvicorn.run(
         "ai_gateway.app:get_app",
         host=config.fastapi.api_host,
