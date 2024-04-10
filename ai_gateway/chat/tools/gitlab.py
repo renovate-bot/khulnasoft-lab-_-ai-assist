de57802d76ef852d03829e88a31a5f9b45e24a52
Alexander Chueshev <achueshev@gitlab.com>
2024-04-10 21:03:20 +0200

    fix: linter errors

--- ai_gateway/chat/tools/gitlab.py
+++ ai_gateway/chat/tools/gitlab.py
@@ -13,7 +13,7 @@ class CiEditorAssistant(BaseRemoteTool):
     name: str = "ci_editor_assistant"
     resource: str = "ci editor answers"
 
-    description: str = """Useful tool when you need to provide suggestions regarding anything related 
+    description: str = """Useful tool when you need to provide suggestions regarding anything related
         to ".gitlab-ci.yml" file. It helps with questions related to deploying code, configuring CI/CD pipelines,
         defining CI jobs, or environments."""
 
@@ -28,8 +28,8 @@ class IssueReader(BaseRemoteTool):
     name: str = "issue_reader"
     resource: str = "issues"
 
-    description: str = """Gets the content of the current issue (also referenced as this or that) the user sees 
-        or a specific issue identified by an ID or a URL. In this context, word `issue` means core building block 
+    description: str = """Gets the content of the current issue (also referenced as this or that) the user sees
+        or a specific issue identified by an ID or a URL. In this context, word `issue` means core building block
         in GitLab that enable collaboration, discussions, planning and tracking of work.
         Action Input for this tool should be the original question or issue identifier."""
 
@@ -60,8 +60,8 @@ class EpicReader(BaseRemoteTool):
     resource: str = "epics"
 
     description: str = """Useful tool when you need to retrieve information about a specific epic.
-        In this context, word `epic` means high-level building block in GitLab that encapsulates high-level plans 
-        and discussions. Epic can contain multiple issues. Action Input for this tool should be the original 
+        In this context, word `epic` means high-level building block in GitLab that encapsulates high-level plans
+        and discussions. Epic can contain multiple issues. Action Input for this tool should be the original
         question or epic identifier."""
 
     example: str = """Question: Please identify the author of &123 epic.
