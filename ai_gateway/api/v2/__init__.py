01f65e5eadb0fd36a07a14b6cd7f4aae57430f60
Alexander Chueshev <achueshev@gitlab.com>
2024-04-15 10:50:52 +0200

    chore: do not add the new api endpoint when AIGW_DUOCHAT_EXPERIMENTAL is false

--- ai_gateway/api/v2/__init__.py
+++ ai_gateway/api/v2/__init__.py
@@ -1,3 +1,6 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import APIRouter
 
 from ai_gateway.api.v2 import chat, code
@@ -8,4 +11,8 @@ api_router = APIRouter()
 
 # We don't include the `code` prefix here, as we need to support the legacy `/completions` endpoint.
 api_router.include_router(code.router, tags=["completions"])
-api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+
+load_dotenv()
+if os.environ.get("AIGW_DUOCHAT_EXPERIMENTAL", False):
+    # TODO: Remove this env variable once the agent executor is accepted for prod.
+    api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
