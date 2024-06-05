5e17f65cec11f1295c49c75b33fa4724abaa7b3d
Anatoli Babenia <anatoli@rainforce.org>
2024-06-05 08:45:38 +0000

    fix: validation script "unbound variable" errors

--- scripts/ingest/gitlab-docs/steps/validate.sh
+++ scripts/ingest/gitlab-docs/steps/validate.sh
@@ -1,45 +1,65 @@
 #!/usr/bin/env bash
 
-set -eu
+set -e
 
-echo "GITLAB_DOCS_REPO: ${GITLAB_DOCS_REPO}"
+# -- echo helpers
+RED='\033[0;31m'
+NC='\033[0m'        # no color
+
+red() {
+  declare arg1="$1"
+  echo -e "${RED}$arg1${NC}"
+}
+# /- echo helpers
+
+
+fail=0
 
 if [ -z "$GITLAB_DOCS_REPO" ]; then
-    echo "GITLAB_DOCS_REPO is empty!"
-    exit 1
+    red "GITLAB_DOCS_REPO is empty!"
+    fail=1
+else
+    echo "GITLAB_DOCS_REPO: ${GITLAB_DOCS_REPO}"
 fi
 
-echo "GITLAB_DOCS_REPO_REF: ${GITLAB_DOCS_REPO_REF}"
 
 if [ -z "$GITLAB_DOCS_REPO_REF" ]; then
-    echo "GITLAB_DOCS_REPO_REF is empty!"
-    exit 1
+    red "GITLAB_DOCS_REPO_REF is empty!"
+    fail=1
+else
+    echo "GITLAB_DOCS_REPO_REF: ${GITLAB_DOCS_REPO_REF}"
 fi
-
-echo "GITLAB_DOCS_CLONE_DIR: ${GITLAB_DOCS_CLONE_DIR}"
 
 if [ -z "$GITLAB_DOCS_CLONE_DIR" ]; then
-    echo "GITLAB_DOCS_CLONE_DIR is empty!"
-    exit 1
+    red "GITLAB_DOCS_CLONE_DIR is empty!"
+    fail=1
+else
+    echo "GITLAB_DOCS_CLONE_DIR: ${GITLAB_DOCS_CLONE_DIR}"
 fi
-
-echo "GITLAB_DOCS_JSONL_EXPORT_PATH: ${GITLAB_DOCS_JSONL_EXPORT_PATH}"
 
 if [ -z "$GITLAB_DOCS_JSONL_EXPORT_PATH" ]; then
-    echo "GITLAB_DOCS_JSONL_EXPORT_PATH is empty!"
-    exit 1
+    red "GITLAB_DOCS_JSONL_EXPORT_PATH is empty!"
+    fail=1
+else
+    echo "GITLAB_DOCS_JSONL_EXPORT_PATH: ${GITLAB_DOCS_JSONL_EXPORT_PATH}"
 fi
-
-echo "GCP_PROJECT_NAME: ${GCP_PROJECT_NAME}"
 
 if [ -z "$GCP_PROJECT_NAME" ]; then
-    echo "GCP_PROJECT_NAME is empty!"
-    exit 1
+    red "GCP_PROJECT_NAME is empty!"
+    fail=1
+else
+    echo "GCP_PROJECT_NAME: ${GCP_PROJECT_NAME}"
 fi
 
-echo "SEARCH_APP_NAME: ${SEARCH_APP_NAME}"
-
 if [ -z "$SEARCH_APP_NAME" ]; then
-    echo "SEARCH_APP_NAME is empty!"
+    red "SEARCH_APP_NAME is empty!"
+    fail=1
+else
+    echo "SEARCH_APP_NAME: ${SEARCH_APP_NAME}"
+fi
+
+if [ $fail == 1 ]; then
+    echo
+    red "Validation failed."
     exit 1
 fi
