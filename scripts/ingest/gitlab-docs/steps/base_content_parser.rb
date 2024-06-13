d7587dba3e7d59ef17c580cd7577961f57b16430
Anatoli Babenia <anatoli@rainforce.org>
2024-06-13 05:06:17 +0000

    refactor: simplify Ruby code in ingestion

--- scripts/ingest/gitlab-docs/steps/base_content_parser.rb
+++ scripts/ingest/gitlab-docs/steps/base_content_parser.rb
@@ -7,19 +7,23 @@ module Gitlab
   module Llm
     module Embeddings
       module Utils
+        MAX_CHARS_PER_EMBEDDING = 1500
+        MIN_CHARS_PER_EMBEDDING = 100
+        METADATA_REGEX = /\A---(?<metadata>.*?)---/m
+
         class BaseContentParser
           attr_reader :max_chars_per_embedding, :min_chars_per_embedding, :root_url
 
-          def initialize(min_chars_per_embedding, max_chars_per_embedding, root_url:)
-            @max_chars_per_embedding = max_chars_per_embedding
-            @min_chars_per_embedding = min_chars_per_embedding
+          def initialize(root_url)
+            @max_chars_per_embedding = MAX_CHARS_PER_EMBEDDING
+            @min_chars_per_embedding = MIN_CHARS_PER_EMBEDDING
             @root_url = root_url
           end
 
-          def parse_and_split(content, source_name, source_type)
+          def parse_and_split(content, source_name)
             items = []
             md5sum = ::OpenSSL::Digest::SHA256.hexdigest(content)
-            content, metadata = parse_content_and_metadata(content, md5sum, source_name, source_type)
+            content, metadata = parse_content_and_metadata(content, md5sum, source_name)
 
             split_by_newline_positions(content) do |text|
               next if text.nil?
@@ -33,10 +37,10 @@ module Gitlab
             items
           end
 
-          def parse_content_and_metadata(content, md5sum, source_name, source_type)
-            match = content.match(metadata_regex)
+          def parse_content_and_metadata(content, md5sum, source_name)
+            match = content.match(METADATA_REGEX)
             metadata = if match
-                         metadata = YAML.safe_load(content.match(metadata_regex)[:metadata])
+                         metadata = YAML.safe_load(content.match(METADATA_REGEX)[:metadata])
                          content = match.post_match.strip
                          metadata
                        else
@@ -46,8 +50,8 @@ module Gitlab
             metadata['title'] = title(content)
             metadata['md5sum'] = md5sum
             metadata['source'] = source_name
-            metadata['source_type'] = source_type
-            metadata['source_url'] = url(source_name, source_type)
+            metadata['source_type'] = 'doc'
+            metadata['source_url'] = url(source_name)
 
             [content, metadata]
           end
@@ -85,12 +89,9 @@ module Gitlab
             end
           end
 
-          def url(source_name, source_type)
-            return unless source_name
-            return unless source_type == 'doc'
-
+          def url(source_name)
             page = source_name.gsub('/doc/', '').gsub('.md', '')
-            help_page_url(page)
+            "#{root_url}/#{page}"
           end
 
           def title(content)
@@ -101,16 +102,6 @@ module Gitlab
             return unless match && match[:title]
 
             match[:title].gsub(/\*\*\(.+\)\*\*$/, '').strip
-          end
-
-          private
-
-          def metadata_regex
-            /\A---(?<metadata>.*?)---/m
-          end
-
-          def help_page_url(page)
-            "#{root_url}/#{page}"
           end
         end
       end
