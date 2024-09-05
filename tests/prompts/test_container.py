d358b9213883e2131e13df64eea19490bcf9457a
Alejandro Rodríguez <alejandro@gitlab.com>
2024-09-05 08:24:32 +0000

    refactor: support template inclusion in prompts

--- tests/prompts/test_container.py
+++ tests/prompts/test_container.py
@@ -6,6 +6,7 @@ import pytest
 from dependency_injector import containers, providers
 from pydantic_core import Url
 
+from ai_gateway.chat.agents.react import ReActAgent
 from ai_gateway.config import Config
 from ai_gateway.prompts import Prompt
 from ai_gateway.prompts.registry import LocalPromptRegistry
@@ -41,6 +42,13 @@ def test_container(mock_container: containers.DeclarativeContainer):
                 name=str(model_name), endpoint=Url("http://localhost:4000"), provider=""
             )
 
-        assert isinstance(
-            registry.get(str(prompt_id), model_metadata=model_metadata), Prompt
-        )
+        prompt = registry.get(str(prompt_id), model_metadata=model_metadata)
+        assert isinstance(prompt, Prompt)
+
+        if isinstance(prompt, ReActAgent):
+            prompt_template = prompt.bound.middle[0]
+        else:
+            prompt_template = prompt.bound.first
+
+        # Check that the messages are populated correctly
+        assert len(prompt_template.format_messages()) > 0
